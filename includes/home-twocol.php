4576929b954838c8f15a77add9a378d0fc7e2edb
Specter <Specter.hi@gmail.com>
2012-09-28 13:39:07 +0800

    双栏简洁化

--- includes/home-twocol.php
+++ includes/home-twocol.php
@@ -34,15 +34,11 @@
 						<h2 class="entry-title"><a href="<?php the_permalink(); ?>" title="<?php printf( esc_attr__( 'Permalink to %s', 'soulol' ), the_title_attribute( 'echo=0' ) ); ?>" rel="bookmark"><?php the_title(); ?></a></h2>
 						
 						<div class="entry-meta">
+						<div class="clear:after"></div>
 							<span class="meta-date"><?php the_time(get_option('date_format')); ?></span>
-							<span class="meta-sep">|</span>
-							<span class="meta-comments"><?php comments_popup_link( __( '0 Comment', 'soulol' ), __( '1 Comment', 'soulol' ), __( '% Comments', 'soulol' ) ); ?></span>
 						</div> <!--end .entry-meta-->
-				
-						<div class="entry-excerpt">
-							<?php the_excerpt( __( 'Read more <span class="meta-nav">&rarr;</span>', 'soulol' ) ); ?>
-						</div> <!--end .entry-excerpt-->
 					</li> <!--end .first-->
+					<div class="clear:after"></div>
 				<?php } else {
 	                echo '<li class="catlist"><a href="'.get_permalink($post->ID).'" rel="bookmark">'.$post->post_title.'</a></li>';
 	            }
