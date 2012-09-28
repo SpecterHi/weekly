c4103ef31d52684270cc6a07130c1d8615590f24
Specter <Specter.hi@gmail.com>
2012-09-28 11:39:34 +0800

    带分类的loop，分类调用字体

--- includes/loop.php
+++ includes/loop.php
@@ -1,5 +1,5 @@
 <div id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 
-		<h3 class="entry-title"><a href="<?php the_permalink(); ?>" title="<?php printf( esc_attr__( 'Permalink to %s', 'soulol' ), the_title_attribute( 'echo=0' ) ); ?>" rel="bookmark"><?php the_title(); ?></a></h2>
+		<h4 class="entry-title"><a href="<?php the_permalink(); ?>" title="<?php printf( esc_attr__( 'Permalink to %s', 'soulol' ), the_title_attribute( 'echo=0' ) ); ?>" rel="bookmark"><?php the_title(); ?></a></h2>
 	
 </div> <!-- end #post -->
